9de43518c1682d57479556ed1b160008b084cc23
atomic677 <mehraz56@atomicmail.io>
2025-12-11 10:18:22 +0600

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,8 @@
-# Which nixpkgs channel to use
-  channel = "stable-24.05"; # or "unstable"
 
-  # Packages to be installed in the development environment
+   
+   { pkgs, ... }: {
+  channel = "stable-24.05";
+
   packages = with pkgs; [
     unzip
     openssh
@@ -13,30 +14,24 @@
     qemu
   ];
 
-  # Environment variables for the workspace
   env = {
-    # Example: set default editor
     EDITOR = "nano";
   };
 
   idx = {
-    # Extensions from https://open-vsx.org (use "publisher.id")
     extensions = [
       "Dart-Code.flutter"
       "Dart-Code.dart-code"
     ];
 
     workspace = {
-      # Runs when a workspace is first created
       onCreate = { };
-
-      # Runs each time the workspace is (re)started
       onStart = { };
     };
 
-    # Disable previews completely
     previews = {
       enable = false;
     };
   };
 }
+
